171a65153a3d76b04041aa54b06f3542af1cf849
Andrew Houle <andrewhoule@gmail.com>
2014-04-11 16:57:45 -0400

    getting rid of old config file

--- stylesheets/sass/config.rb
+++
@@ -1,8 +0,0 @@
-http_path = "/"
-css_dir = "stylesheets/css"
-sass_dir = "stylesheets/sass"
-images_dir = "images"
-javascripts_dir = "js"
-fonts_dir = "fonts"
-output_style = :compressed
-color_output = false
