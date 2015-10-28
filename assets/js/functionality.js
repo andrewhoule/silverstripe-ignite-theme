bad506c51f5023df184e7a6e451cce64791e9e11
Andrew Houle <andrewhoule@gmail.com>
2015-10-28 10:17:48 -0400

    Adding some logic for responsive images

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
 
+
+/* ==========================================================================
+   Adding some logic to deal with wrapping text around floating image on responsive design. AKA Breathing room.
+   ========================================================================== */
+
+var resizeTimer;
+$(window).resize(function() {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+   var colsize = $('.main-content').width();
+    $('.main-content img').each(function() { 
+    if( ($(this).width() + 400) >= colsize ) {
+      $(this).addClass('left-alone');
+    }
+    else {
+      $(this).removeClass('left-alone');
+    }
+   });
+  }, 200);
+});
+var colsize = $('.main-content').width();
+$('.main-content img').each(function() { 
+  if( ($(this).width() + 400) >= colsize ) {
+    $(this).addClass('left-alone');
+  }
+});
+
+
 }); // End doc ready
