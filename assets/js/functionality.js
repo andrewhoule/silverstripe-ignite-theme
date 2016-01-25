50d3160e5bd5104c42b99dd4c8d3bfbcc8cbc581
Andrew Houle <andrewhoule@gmail.com>
2016-01-25 09:02:35 -0500

    Fixing breathing room stuff for images

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -5,26 +5,24 @@ $(document).ready(function(){
    Adding some logic to deal with wrapping text around floating image on responsive design. AKA Breathing room.
    ========================================================================== */
 
-var resizeTimer;
-$(window).resize(function() {
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(function() {
-   var colsize = $('.main-content').width();
-    $('.main-content img').each(function() { 
-    if( ($(this).width() + 400) >= colsize ) {
+function imageBreathingRoom() {
+  var colsize = $('.main-content').width();
+  $('.main-content img').each(function() { 
+      if(($(this).width() + 400) >= colsize ) {
       $(this).addClass('left-alone');
     }
     else {
       $(this).removeClass('left-alone');
     }
-   });
+  });
+}
+imageBreathingRoom();
+var resizeTimer;
+$(window).resize(function() {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    imageBreathingRoom();
   }, 200);
-});
-var colsize = $('.main-content').width();
-$('.main-content img').each(function() { 
-  if( ($(this).width() + 400) >= colsize ) {
-    $(this).addClass('left-alone');
-  }
 });
 
 
