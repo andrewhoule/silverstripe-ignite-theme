2f184b1ea753c03da99db995fc6d6bc8f8072959
Andrew Houle <andrewhoule@gmail.com>
2016-07-27 13:05:41 -0400

    SCSS cleanup

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
    ========================================================================== */
 
 (function($) {
-  $( 'p:empty' ).remove();
+  $('p:empty').remove();
 })(jQuery);
 
 
