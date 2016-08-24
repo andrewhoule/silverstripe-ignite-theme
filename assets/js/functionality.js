6b59bcd58ebc64b2a682f6f1c198a28453d3d26f
Andrew Houle <andrewhoule@gmail.com>
2016-08-24 09:22:02 -0400

    Adding SVG4Everybody

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -50,3 +50,14 @@
     $('p:empty').remove();
   });
 })(jQuery);
+
+
+/* ==========================================================================
+   Get SVG working for older versions of IE / * Requires SVG4Everybody
+   ========================================================================== */
+
+(function($) {
+  $(document).ready(function(){
+    svg4everybody();
+  });
+})(jQuery);
